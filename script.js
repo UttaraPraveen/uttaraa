f48041a952988d6591710e71ccb536a63b4a620f
UttaraPraveen <117985781+UttaraPraveen@users.noreply.github.com>
2022-11-13 14:37:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("displayAnswer");
+let buttons = Array.from(document.getElementsByClassName("button"));
+console.log(0);
+buttons.map( button => { 
+    button.addEventListener('click',(e) => {
+        switch(e.target.innerText){
+            case 'C': display.innerText= "";
+            break;
+            case '=': display.innerText = eval(display.innerText);
+            break;
+            case '←': display.innerText = display.innerText.slice(0, -1);
+            break;
+            default: display.innerText += e.target.innerText;
+        }
+    });
+}
+);
